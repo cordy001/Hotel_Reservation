c4bf153f231a4f9d9a499d7e969c5884c6a84b26
Your GitHub Username <mglgain@gmail.com>
2025-04-07 19:45:46 +0800

    Commiting

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+document.getElementById("form_id").addEventListener('submit', function() {
+
+    const name_id = document.getElementById("name").value || null
+    const age_id = document.getElementById("age").value || null
+    const address_id = document.getElementById("address").value || null
+    const spa_id = document.getElementById("spa").value || null
+    const massage_id = document.getElementById("massage").value || null
+    const breakfast_id = document.getElementById("breakfast").value || null
+    const regular_id = document.getElementById("regular").value || null
+    const senior_id = document.getElementById("senior").value || null
+    const toa_id = document.getElementById("toa").value || null
+    const numdays_id = document.getElementById("numdays").value || null
+    const downpayment_id = document.getElementById("downpayment"). value || null
+
+    alert("Name: ", name_id)
+
+
+})
